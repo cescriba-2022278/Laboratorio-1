beb057e869fd82ed66fd5d16865430ea97e05d58
cescriba-2022278 <cescriba-2022278@kinal.edu.gt>
2024-01-24 22:02:09 -0600

    css y html kinal mejorados

--- assets/scripts/kinal.js
+++ assets/scripts/kinal.js
@@ -1,41 +1,41 @@
 $(document).ready(function () {
-    $("#slider").fadeIn("slow").delay(200);
-    $("#sticker").sticky({ topSpacing: 0 });
-});
-    
-$(document).ready(function () {
-    $("a").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-  
-        var hash = this.hash;
-  
-        $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top
-          },
-          1000,
-          function () {
-            window.location.hash = hash;
-          }
-        );
-      }
-    });
+  $("#slider").fadeIn("slow").delay(200);
+  $("#sticker").sticky({ topSpacing: 0 });
 });
   
 $(document).ready(function () {
-    $(window).trigger("scroll");
-    $(window).bind("scroll", function () {
-      var pixels = 50; //pixeles abajo
-      if ($(window).scrollTop() > pixels) {
-        $(".nav").addClass("fixed");
-        $(".menu a").css({ color: "#354757" });
-        $(".logo").css({ color: "#354757" });
-      } else {
-        $(".nav").removeClass("fixed");
-        $(".menu a").css({ color: "#BABCBD" });
-        $(".logo").css({ color: "#BABCBD" });
-      }
-    });
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        1000,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(window).trigger("scroll");
+  $(window).bind("scroll", function () {
+    var pixels = 50;
+    if ($(window).scrollTop() > pixels) {
+      $(".nav").addClass("fixed");
+      $(".menu a").css({ color: "#354757" });
+      $(".logo").css({ color: "#354757" });
+    } else {
+      $(".nav").removeClass("fixed");
+      $(".menu a").css({ color: "#BABCBD" });
+      $(".logo").css({ color: "#BABCBD" });
+    }
+  });
 });
   
